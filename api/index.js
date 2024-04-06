d522b0b4c57ab6e881a25d95623d9ac83bb82aad
Franco Feuilles <francofeuilles@gmail.com>
2024-04-06 15:43:18 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,6 @@ app.use(express.static(path.join(__dirname, "../express")));
 app.get("/express", (req, res) => res.sendFile(path.join(__dirname, "../express/index.html")));
 app.get("/dom", (req, res) => res.sendFile(path.join(__dirname, "../public/dom/index.html")));
 app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
-app.use(express.static('public'))
 
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
