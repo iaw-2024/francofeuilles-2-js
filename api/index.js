fe5ff21b7aa70601e1c8711658bb78bdc4357246
Franco Feuilles <francofeuilles@gmail.com>
2024-04-08 11:12:51 -0300

    Agrega implementación cliente-servidor

--- api/index.js
+++ api/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
+
+const dataPath = path.join(__dirname, "data.json");
+const data = JSON.parse(fs.readFileSync(dataPath, "utf8"));
 
 // Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/express", (req, res) => res.sendFile(path.join(__dirname, "../public/express/index.html")));
 app.get("/dom", (req, res) => res.sendFile(path.join(__dirname, "../public/dom/index.html")));
-app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
+app.get("/cliente_servidor", (req, res) => res.sendFile(path.join(__dirname, "../public/cliente_servidor/index.html")));
+app.get("/cliente_servidor/data", (req, res) => res.json(data))
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
 
---
+++ public/cliente_servidor/data-loader.js
@@ -0,0 +1,46 @@
+function cargarDatosEnTabla(compras) {
+    var tbody = document.querySelector('#cryptoTable tbody');
+    limpiarTabla(tbody); // Limpiar la tabla antes de agregar nuevos datos
+    compras.forEach(compra => agregarCompraATabla(compra, tbody));
+}
+
+function agregarCompraATabla(compra, tbody) {
+    var newRow = document.createElement('tr');
+    agregarCelda(newRow, compra.criptomoneda);
+    agregarCelda(newRow, compra.precio);
+    agregarCelda(newRow, compra.cantidad);
+    agregarCelda(newRow, compra.direccion);
+    tbody.appendChild(newRow);
+}
+
+function agregarCelda(fila, contenido) {
+    var cell = document.createElement('td');
+    cell.textContent = contenido;
+    fila.appendChild(cell);
+}
+
+function limpiarTabla(tbody) {
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('/cliente_servidor/data')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('No se pudieron obtener los datos.');
+            }
+            return response.json();
+        })
+        .then(compras => {
+            if (Array.isArray(compras)) {
+                cargarDatosEnTabla(compras);
+            } else {
+                throw new Error('Los datos recibidos no son válidos.');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error.message);
+        });
+});
