b05536a6fde5f6003929c0862f336e6bbfdad8b1
Franco Feuilles <francofeuilles@gmail.com>
2024-04-09 17:14:42 -0300

    Cambie la parte de express para que sea un render de un template con mi data.json

--- api/index.js
+++ api/index.js
@@ -6,13 +6,16 @@ const fs = require("fs");
 const dataPath = path.join(__dirname, "data.json");
 const data = JSON.parse(fs.readFileSync(dataPath, "utf8"));
 
-// Middleware para servir archivos estáticos
-app.use(express.static(path.join(__dirname, "../public")));
+app.set("views", path.join(__dirname, "view"));
+app.set("view engine", "ejs");
 
-app.get("/express", (req, res) => res.sendFile(path.join(__dirname, "../public/express/index.html")));
+app.get("/express", (req, res) => {res.render("view", { data: data });}); 
 app.get("/dom", (req, res) => res.sendFile(path.join(__dirname, "../public/dom/index.html")));
 app.get("/cliente_servidor", (req, res) => res.sendFile(path.join(__dirname, "../public/cliente_servidor/index.html")));
 app.get("/cliente_servidor/data", (req, res) => res.json(data))
+
+// Middleware para servir archivos estáticos
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
 
